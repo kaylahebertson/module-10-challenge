f126ea6ed0d8e436e01c2ba1b74742082b7ce04a
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-12-09 15:29:55 -0700

    more outline

--- Develop/index.ts
+++ Develop/index.ts
@@ -1,5 +1,60 @@
 import inquirer from 'inquirer';
+import { Db } from './db/index.js';
 
 const db = new Db();
 
 init();
+
+function init() {
+    mainPage();
+}
+
+function mainPage() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'Select an action',
+                choices: [
+                    'List all employees',
+                    'List all departments',
+                    'List all roles',
+                    'Add employee',
+                    'Add department',
+                    'Add role',
+                    'Update employee role',
+                    'Quit'
+                ]
+            }
+        ])
+        .then(async answers => {
+            switch (answers.action) {
+                case 'List all employees':
+                    listEmployees();
+                    break;
+                case 'List all departments':
+                    listDepartments();
+                    break;
+                case 'List all roles':
+                    listRoles();
+                    break;
+                case 'Add employee':
+                    addEmployee();
+                    break;
+                case 'Add department':
+                    addDepartment();
+                    break;
+                case 'Add role':
+                    addRole();
+                    break;
+                case 'Update employee role':
+                    updateEmployeeRole();
+                    break;
+                case 'Quit':
+                    process.exit();
+                    break;
+            }
+        });
+}
+
