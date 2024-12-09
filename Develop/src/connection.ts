99ef7934c13697c31c5665b396f30745b837aa9a
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-12-09 15:18:31 -0700

    connecting to DB

---
+++ Develop/src/connection.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: 'localhost',
+    port: 5432,
+    database: process.env.DB_NAME
+});
+
+const connectDB = async () => {
+    try {
+        await pool.connect();
+        console.log('Database connected');
+    } catch (error) {
+        console.error('Database connection failed');
+    }
+};
+
+export { pool, connectDB };
