3a2c2aa813e2756713d447ad1af983dc3df37310
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-12-09 15:33:24 -0700

    add new department, employee, role funcitons

--- Develop/index.ts
+++ Develop/index.ts
@@ -81,3 +81,82 @@ function listRoles() {
             mainPage();
         });
 }
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'Enter first name'
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'Enter last name'
+            },
+            {
+                type: 'input',
+                name: 'role_id',
+                message: 'Enter role ID'
+            },
+            {
+                type: 'input',
+                name: 'manager_id',
+                message: 'Enter manager ID'
+            }
+        ])
+        .then(answers => {
+            db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)', [answers.first_name, answers.last_name, answers.role_id, answers.manager_id])
+                .then(() => {
+                    console.log('Employee added');
+                    mainPage();
+                });
+        });
+}
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Enter department name'
+            }
+        ])
+        .then(answers => {
+            db.query('INSERT INTO department (name) VALUES ($1)', [answers.name])
+                .then(() => {
+                    console.log('Department added');
+                    mainPage();
+                });
+        });
+}
+
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Enter role title'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'Enter role salary'
+            },
+            {
+                type: 'input',
+                name: 'department_id',
+                message: 'Enter department ID'
+            }
+        ])
+        .then(answers => {
+            db.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [answers.title, answers.salary, answers.department_id])
+                .then(() => {
+                    console.log('Role added');
+                    mainPage();
+                });
+        });
+}
